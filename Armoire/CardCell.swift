0893124998dfe364b5caeee27e16941a8a24fc11
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-05-16 23:56:38 +0530

    suggestion Table View completed

---
+++ Armoire/CardCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardCell.swift
+//  Armoire
+//
+//  Created by Abhay singh on 16/05/24.
+//
+
+import UIKit
+
+class CardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
