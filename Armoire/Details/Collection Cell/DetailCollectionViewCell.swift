41ad566098f9071d2c3bf7fc34e0536e1abab705
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-05-21 01:20:19 +0530

    added skeleton for details

---
+++ Armoire/Details/Collection Cell/DetailCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailCollectionViewCell.swift
+//  Armoire
+//
+//  Created by Abhay singh on 20/05/24.
+//
+
+import UIKit
+
+class DetailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Armoire/Details/Table Cell/CollectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionTableViewCell.swift
+//  Armoire
+//
+//  Created by Abhay singh on 20/05/24.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
