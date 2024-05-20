3568c1deca95fcba570e086c832f21763a553545
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-05-20 09:56:04 +0530

    added view files for details section for clothes in wardrobe

---
+++ Armoire/Collection Cell/DetailsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsCollectionViewCell.swift
+//  Armoire
+//
+//  Created by Abhay singh on 19/05/24.
+//
+
+import UIKit
+
+class DetailsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
