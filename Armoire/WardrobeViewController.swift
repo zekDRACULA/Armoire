905a74d32d85daf4b0012fa04c49286aed996c04
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-05-20 23:40:00 +0530

    Revert "added Camera and Gallery Action"
    This reverts commit 064b1c490deac375bc72ed475468459694cbc303.

--- Armoire/WardrobeViewController.swift
+++ Armoire/WardrobeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WardrobeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class WardrobeViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -24,78 +24,7 @@ class WardrobeViewController: UIViewController, UIImagePickerControllerDelegate,
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
-        
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        let alertController = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alertController.addAction(cancelAction)
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {action in imagePicker.sourceType = .camera
-                self.present(imagePicker, animated: true, completion: nil)})
-            alertController.addAction(cameraAction)
-            
-        }
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: {action in imagePicker.sourceType = .photoLibrary
-                self.present(imagePicker, animated: true, completion: nil)})
-            alertController.addAction(photoLibraryAction)
-        }
-        
-        alertController.popoverPresentationController?.sourceItem = sender
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    
-    @IBAction func DotButtonTapped(_ sender: UIBarButtonItem) {
-//        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-//
-//        // Main Menu Items
-//        alertController.addAction(UIAlertAction(title: "Option 1", style: .default, handler: { _ in
-//            // Handle Option 1 action
-//        }))
-//        alertController.addAction(UIAlertAction(title: "Option 2", style: .default, handler: { _ in
-//            // Handle Option 2 action
-//        }))
-//
-//        // Submenu Item
-//        let submenuAction = UIAlertAction(title: "More Options", style: .default, handler: { _ in
-//            self.showSubMenu()
-//        })
-//        alertController.addAction(submenuAction)
-//
-//        // Cancel Action
-//        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-//
-//        present(alertController, animated: true)
-    }
 
-    func showSubMenu() {
-        let submenuAlert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        // Submenu Items
-        submenuAlert.addAction(UIAlertAction(title: "Sub Option 1", style: .default, handler: { _ in
-            // Handle Sub Option 1 action
-        }))
-        submenuAlert.addAction(UIAlertAction(title: "Sub Option 2", style: .default, handler: { _ in
-            // Handle Sub Option 2 action
-        }))
-
-        // Cancel Action
-        submenuAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-
-        present(submenuAlert, animated: true)
-    }
-    
-    
-    
-    
-    
     /*
     // MARK: - Navigation
 
