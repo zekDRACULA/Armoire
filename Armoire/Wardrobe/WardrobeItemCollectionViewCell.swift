21cacba89e011d4c241adc05f6039f14ab3e0251
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-05-24 02:00:37 +0530

    fixed wardrobe tab

---
+++ Armoire/Wardrobe/WardrobeItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WardrobeItemCollectionViewCell.swift
+//  Armoire
+//
+//  Created by Hitesh Rupani on 23/05/24.
+//
+
+import UIKit
+
+class WardrobeItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var itemImage: UIImageView!
+}
