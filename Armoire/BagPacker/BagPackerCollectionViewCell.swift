e905738cf5061d9c14642674c1ea4ed8faa6c2a8
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-05-25 10:30:05 +0530

    Bag Packer

---
+++ Armoire/BagPacker/BagPackerCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BagPackerCollectionViewCell.swift
+//  Armoire
+//
+//  Created by Abhay singh on 24/05/24.
+//
+
+import UIKit
+
+class BagPackerCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
