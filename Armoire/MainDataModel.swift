5c146adbef80405b4c620fa741e791f6396dbd61
Kanika Gupta <kanikagupta140203@gmail.com>
2024-05-30 23:21:18 +0530

    homeModel deleted

--- Armoire/MainDataModel.swift
+++ Armoire/MainDataModel.swift
@@ -19,6 +19,27 @@ class MainDataModel {
 //        Apparel(image: UIImage(named: "Image_4")!, id: 4, color: .blue, pattern: .solid, type: .bottom, tag: ["Party", "Top"]),
 //        Apparel(image: UIImage(named: "Image_5")!, id: 5, color: .blue, pattern: .solid, type: .bottom, tag: ["Party", "Top"])
 //    ]
+    enum Pattern {
+        case solid, stripes, checks, printed, dots, floral
+    }
+
+    static var tags: [String] = ["Summer", "Winter", "Top", "Bottom", "Dress"]
+
+    static var Wardrobe: [Apparel] = [
+        Apparel(image: UIImage(named: "Image_1")!, id: 1, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_2")!, id: 2, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_3")!, id: 3, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_4")!, id: 4, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_5")!, id: 5, color: .black, pattern: .solid, tag: ["Party", "Top"])
+    ]
+
+    static var Favourites: [Apparel] = [
+        Apparel(image: UIImage(named: "Image_1")!, id: 1, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_2")!, id: 2, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_3")!, id: 3, color: .black, pattern: .solid, tag: ["Party", "Top"])
+    ]
+    
+    static var outfit:[Outfit] = [Outfit(top: Wardrobe[0], bottom: Wardrobe[1]), Outfit(top: Wardrobe[0], bottom: Wardrobe[1]), Outfit(top: Wardrobe[0], bottom: Wardrobe[1]), Outfit(top: Wardrobe[0], bottom: Wardrobe[1])]
 
 //    static var favourites: [Apparel] = wardrobe.filter{$0.isFavourite}
     
