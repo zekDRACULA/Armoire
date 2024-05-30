b651d4fb321eaba47dc1dd7991cb0e6b297701e9
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-05-30 09:27:04 +0530

    update data model

--- Armoire/MainDataModel.swift
+++ Armoire/MainDataModel.swift
@@ -35,19 +35,21 @@ class MainDataModel {
     static var tags: [String] = ["Summer", "Winter", "Top", "Bottom", "Dress"]
 
     static var Wardrobe: [Apparel] = [
-        Apparel(image: UIImage(named: "Image_1")!, id: 1, color: .black, pattern: .solid, tag: ["Party", "Top"]),
-        Apparel(image: UIImage(named: "Image_2")!, id: 2, color: .black, pattern: .solid, tag: ["Party", "Top"]),
-        Apparel(image: UIImage(named: "Image_3")!, id: 3, color: .black, pattern: .solid, tag: ["Party", "Top"]),
-        Apparel(image: UIImage(named: "Image_4")!, id: 4, color: .black, pattern: .solid, tag: ["Party", "Top"]),
-        Apparel(image: UIImage(named: "Image_5")!, id: 5, color: .black, pattern: .solid, tag: ["Party", "Top"])
+        Apparel(image: UIImage(named: "Image_1")!, id: 1, color: .black, pattern: .solid, type: .top, tag: ["Party", "Top"], isFavourite: true),
+        Apparel(image: UIImage(named: "Image_2")!, id: 2, color: .brown, pattern: .solid, type: .top, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_3")!, id: 3, color: .black, pattern: .solid, type: .top, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_4")!, id: 4, color: .blue, pattern: .solid, type: .top, tag: ["Party", "Top"]),
+        Apparel(image: UIImage(named: "Image_5")!, id: 5, color: .blue, pattern: .solid, type: .top, tag: ["Party", "Top"])
     ]
 
-    static var Favourites: [Apparel] = [
-        Apparel(image: UIImage(named: "Image_1")!, id: 1, color: .black, pattern: .solid, tag: ["Party", "Top"]),
-        Apparel(image: UIImage(named: "Image_2")!, id: 2, color: .black, pattern: .solid, tag: ["Party", "Top"]),
-        Apparel(image: UIImage(named: "Image_3")!, id: 3, color: .black, pattern: .solid, tag: ["Party", "Top"])
-    ]
+//    static var Favourites: [Apparel] = [
+//        Apparel(image: UIImage(named: "Image_1")!, id: 1, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+//        Apparel(image: UIImage(named: "Image_2")!, id: 2, color: .black, pattern: .solid, tag: ["Party", "Top"]),
+//        Apparel(image: UIImage(named: "Image_3")!, id: 3, color: .black, pattern: .solid, tag: ["Party", "Top"])
+//    ]
 
+    static var Favourites: [Apparel] = Wardrobe.filter{$0.isFavourite}
+    
 }
 
 
