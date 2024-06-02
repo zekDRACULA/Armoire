8bf230ea94af2502e578955e46ed2c941f8825d0
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-06-02 12:41:40 +0530

    working

---
+++ Armoire/Login/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Armoire
+//
+//  Created by Abhay singh on 02/06/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+
+    @IBOutlet weak var foregroundView : UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
